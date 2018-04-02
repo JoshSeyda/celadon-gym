580f566b0c8188f97d215b18c5727cf3346dc6f7
Josh Seyda <josh.seyda@gmail.com>
2018-04-02 16:08:28 -0400

    matching up variables

--- main.js
+++ main.js
@@ -79,17 +79,17 @@ function createPokemon(pokemon) {
 
             // move pokemon object to trainer
             title = new Pokemon(name, pic, stat, abil);
-            ashKetchum.roster.push(title);
-            ashKetchum.get(title);
+            pokemonTrainer.pokemon.push(title);
+            pokemonTrainer.get(title);
             counter++;
             let futureRef = ["#one!", "#two!", "#three!", "#four!", "#five!", "#six!", "#seven!", "#eight!", "#nine!", "#ten!", "#eleven!", "#twelve!"];
 
             //waits for ajax to finish, then manipulate DOM
             if (counter === 3) {
-                ashKetchum.all();
-                console.log(ashKetchum);
+                pokemonTrainer.all();
+                console.log(pokemonTrainer);
                 let render = function() {
-                    for (let i = 0; i < ashKetchum.roster.length; i++) {
+                    for (let i = 0; i < pokemonTrainer.pokemon.length; i++) {
                         let reference;
                         if (i === 0) {
                             reference = futureRef[i];
@@ -100,17 +100,17 @@ function createPokemon(pokemon) {
                         } else {};
                         let card = `<div class="carousel-item z-depth-5" href="${reference}"><div class="card large">
 						<div class="card-image waves-effect waves-block waves-light">
-						  <img class="activator" src="${ashKetchum.roster[i].pic}">
+						  <img class="activator" src="${pokemonTrainer.pokemon[i].pic}">
 						</div>
 						<div class="card-content">
-						  <span class="card-title activator grey-text text-darken-4">${ashKetchum.roster[i].name}<i class="material-icons right small">insert_chart</i></span>
+						  <span class="card-title activator grey-text text-darken-4">${pokemonTrainer.pokemon[i].name}<i class="material-icons right small">insert_chart</i></span>
 						</div>
 						<div class="card-reveal">
-						  <span class="card-title grey-text text-darken-4">${ashKetchum.roster[i].name}<i class="material-icons right">close</i></span>
+						  <span class="card-title grey-text text-darken-4">${pokemonTrainer.pokemon[i].name}<i class="material-icons right">close</i></span>
 						 	<p>Stats</p> 
-						  <p>${ashKetchum.roster[i].statsRender()}</p>
+						  <p>${pokemonTrainer.pokemon[i].statsRender()}</p>
 						  <p>Abilities</p>
-						  <p>${ashKetchum.roster[i].abilitiesRender()}</p>
+						  <p>${pokemonTrainer.pokemon[i].abilitiesRender()}</p>
 						</div>
 					  </div>
 					  </div>`;
@@ -123,23 +123,23 @@ function createPokemon(pokemon) {
                 $('.carousel').carousel();
             }
             if (counter > 3) {
-                let index = ashKetchum.roster.length - 1;
+                let index = pokemonTrainer.pokemon.length - 1;
                 console.log(index);
                 console.log(futureRef[index]);
                 let render = function() {
                     let card = `<div class="carousel-item z-depth-5" href="${futureRef[index]}"><div class="card large">
                         <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="${ashKetchum.roster[index].pic}">
+                        <img class="activator" src="${pokemonTrainer.pokemon[index].pic}">
                         </div>
                         <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">${ashKetchum.roster[index].name}<i class="material-icons right small">insert_chart</i></span>
+                        <span class="card-title activator grey-text text-darken-4">${pokemonTrainer.pokemon[index].name}<i class="material-icons right small">insert_chart</i></span>
                         </div>
                         <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">${ashKetchum.roster[index].name}<i class="material-icons right">close</i></span>
+                        <span class="card-title grey-text text-darken-4">${pokemonTrainer.pokemon[index].name}<i class="material-icons right">close</i></span>
                             <p>Stats</p> 
-                        <p>${ashKetchum.roster[index].statsRender()}</p>
+                        <p>${pokemonTrainer.pokemon[index].statsRender()}</p>
                         <p>Abilities</p>
-                        <p>${ashKetchum.roster[index].abilitiesRender()}</p>
+                        <p>${pokemonTrainer.pokemon[index].abilitiesRender()}</p>
                         </div>
                         </div>
                         </div>`;
@@ -148,47 +148,11 @@ function createPokemon(pokemon) {
                 }
                 render();
                 $('.carousel').carousel({
-                    numVisible: ashKetchum.roster.length
+                    numVisible: pokemonTrainer.pokemon.length
                 });
                 let elem = $('.carousel');
                 let instance = M.Carousel.getInstance(elem);
                 instance.set(index);
-                if (counter == 5) {
-                    let modal = ` <div id="modal2" class="modal"><div class="modal-content">
-                    <img src="photos/FireRed_LeafGreen_Professor_Oak.png">
-                    <p>You still have lots to do. </p>
-                    <p>Go into every patch of grass you see and look for Pokémon!</p>
-                </div>
-                <div class="modal-footer">
-                    <audio controls="controls" autoplay>
-                            <source src="audio/palettetown.mp3">
-                            Your browser does not support the <code>audio</code> element.
-                            </audio>
-                              <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
-                          </div>
-                      </div>`;
-                    $('#modalContainer').append(modal);
-                    $('#modal2').modal();
-                    $('#modal2').modal('open');
-                }
-                if (counter == 7) {
-                    let modal = ` <div id="modal3" class="modal"><div class="modal-content">
-                    <img src="photos/FireRed_LeafGreen_Professor_Oak.png">
-                    <p>Good, it's apparent that you're trying hard! </p>
-                    <p>I've given one of my Aides an Itemfinder. Be sure to collect it!</p>
-                </div>
-                <div class="modal-footer">
-                    <audio controls="controls" autoplay>
-                            <source src="audio/pokemon-gym.mp3">
-                            Your browser does not support the <code>audio</code> element.
-                            </audio>
-                              <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
-                          </div>
-                      </div> `;
-                    $('#modalContainer').append(modal);
-                    $('#modal3').modal();
-                    $('#modal3').modal('open');
-                }
             }
         },
         error: function(error) {
